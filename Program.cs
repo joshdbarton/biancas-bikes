4420c120f8a47bda4fca47eb95a143f6020a3b69
Josh Barton <joshdbarton@gmail.com>
2024-02-15 10:48:20 -0500

    updating with connection string env var and rest of build process

--- Program.cs
+++ Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddIdentityCore<UserProfile>(config =>
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 // allows our api endpoints to access the database through Entity Framework Core
-builder.Services.AddNpgsql<BiancasBikesDbContext>(builder.Configuration["BiancasBikesDbConnectionString"]);
+builder.Services.AddNpgsql<BiancasBikesDbContext>(builder.Configuration["POSTGRESQLCONNSTR_biancasbikes"]);
 
 
 var app = builder.Build();
